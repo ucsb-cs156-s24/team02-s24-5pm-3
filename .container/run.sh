5dd9e26a124235af731cde00cbc8872a7966392e
Unics Yu <yulele28@126.com>
2024-04-25 17:56:08 -0700

    [feat] change port mapping

--- .container/run.sh
+++ .container/run.sh
@@ -1,2 +1,2 @@
-docker run -it --name ttt -v "$(pwd)":/app team02 bash
-
+# docker run -it --name ttt -v "$(pwd)":/app team02 bash
+docker run -it --name ttt -v "$(pwd)":/app -p 8080:8080 team02 bash
