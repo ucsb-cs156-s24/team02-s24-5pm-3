9a93020445d7fb31cb701c102e8719a9a54651f1
Tyler <tmcanepa@ucsb.com>
2024-04-30 22:17:51 -0700

    added controller

--- src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
@@ -1,21 +1,16 @@
 package edu.ucsb.cs156.example.controllers;
 
-
 import edu.ucsb.cs156.example.entities.RecommendationRequest;
 import edu.ucsb.cs156.example.entities.UCSBDate;
 import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.RecommendationRequestRepository;
 import edu.ucsb.cs156.example.repositories.UCSBDateRepository;
-
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -29,68 +24,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import javax.validation.Valid;
-
 
 import java.time.LocalDateTime;
 
-
-@Tag(name = "RecommendationRequest")
-@RequestMapping("/api/recommendationrequest")
+@Tag(name = "recommendationrequest")
+@RequestMapping("/api/RecommendationRequest")
 @RestController
 @Slf4j
 public class RecommendationRequestController extends ApiController{
   @Autowired
     RecommendationRequestRepository recommendationRequestRepository;
 
-
-  @Operation(summary= "List all ucsb dates")
-  @PreAuthorize("hasRole('ROLE_USER')")
-  @GetMapping("/all")
-    public Iterable<RecommendationRequest> getAllRecommendationRequests() {
-        Iterable<RecommendationRequest> request = recommendationRequestRepository.findAll();
-        return request;
+    @Operation(summary= "List all recomendation requests")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<RecommendationRequest> allRecommendationRequests() {
+        Iterable<RecommendationRequest> requests = recommendationRequestRepository.findAll();
+        return requests;
     }
 
+    @Operation(summary= "Create a new recommendation request")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public RecommendationRequest postRecommendationRequest(
+            @Parameter(name="requesterEmail") @RequestParam String requesterEmail,
+            @Parameter(name="professorEmail") @RequestParam String professorEmail,
+            @Parameter(name="explanation") @RequestParam String explanation,
+            @Parameter(name="dateRequested") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateRequested,
+            @Parameter(name="dateNeeded") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateNeeded,
+            @Parameter(name="done") @RequestParam boolean done)
+            throws JsonProcessingException {
 
-  @Operation(summary= "Create a new request")
-  @PreAuthorize("hasRole('ROLE_ADMIN')")
-  @PostMapping("/post")
-  public RecommendationRequest postRecommendationRequest(
-          @Parameter(name="requesterEmail") @RequestParam String requesterEmail,
-          @Parameter(name="professorEmail") @RequestParam String professorEmail,
-          @Parameter(name="explanation") @RequestParam String explanation,
-          @Parameter(name="date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("dateRequested") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateRequested,
-          @Parameter(name="date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("dateNeeded") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateNeeded,
-          @Parameter(name="done") @RequestParam boolean done)
-          throws JsonProcessingException {
+        // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+        // See: https://www.baeldung.com/spring-date-parameters
 
+        log.info("dateRequested={}", dateRequested);
 
-      // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-      // See: https://www.baeldung.com/spring-date-parameters
+        RecommendationRequest recommendationRequest = new RecommendationRequest();
+        recommendationRequest.setRequesterEmail(requesterEmail);
+        recommendationRequest.setProfessorEmail(professorEmail);
+        recommendationRequest.setExplanation(explanation);
+        recommendationRequest.setDateRequested(dateRequested);
+        recommendationRequest.setDateNeeded(dateNeeded);
+        recommendationRequest.setDone(done);
 
+        RecommendationRequest savedRecommendationRequest = recommendationRequestRepository.save(recommendationRequest);
 
-      log.info("dateRequested={}", dateRequested);
-      log.info("dateNeeded={}", dateNeeded);
+        return savedRecommendationRequest;
+    }
 
-
-      RecommendationRequest recommendationRequest = new RecommendationRequest();
-      recommendationRequest.setRequesterEmail(requesterEmail);
-      recommendationRequest.setProfessorEmail(professorEmail);
-      recommendationRequest.setExplanation(explanation);
-      recommendationRequest.setDateRequested(dateRequested);
-      recommendationRequest.setDateNeeded(dateNeeded);
-      recommendationRequest.setDone(done);
-
-
-      RecommendationRequest savedRecReq = recommendationRequestRepository.save(recommendationRequest);
-
-
-      return savedRecReq;
-  }
- 
 }
-
-
-
