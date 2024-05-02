560bc2fd1e2f9895d12a1943ce4228a22b9b7c2d
sherpatran <115107544+sherpatran@users.noreply.github.com>
2024-05-02 13:17:02 -0700

    created controller and starting to fix mistakes on original files

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemRevivewController.java
@@ -0,0 +1,69 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.entities.MenuItemReview;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
+import edu.ucsb.cs156.example.repositories.MenuItemReviewRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import java.time.LocalDateTime;
+
+@Tag(name = "MenuItemReview")
+@RequestMapping("/api/menuitemreview")
+@RestController
+@Slf4j
+public class MenuItemRevivewController extends ApiController {
+
+    @Autowired
+    MenuItemReviewRepository MenuItemReviewRepository;
+
+    @Operation(summary = "List all menu item reviews")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<MenuItemReview> allUMenuItemReview() {
+        Iterable<MenuItemReview> reviews = MenuItemReviewRepository.findAll();
+        return reviews;
+    }
+
+    @Operation(summary = "Create a new review")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public MenuItemReview postMenuItemReview(
+            @Parameter(name = "email") @RequestParam String reviewerEmail,
+            @Parameter(name = "stars (from 0 to 5)") @RequestParam int stars,
+            @Parameter(name = "date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime localDateTime,
+            @Parameter(name = "comments") @RequestParam String comments)
+            throws JsonProcessingException {
+
+        log.info("localDateTime={}", localDateTime);
+
+        MenuItemReview menuItemReview = new MenuItemReview();
+        menuItemReview.setReviewerEmail(reviewerEmail);
+        menuItemReview.setStars(stars);
+        menuItemReview.setDateReviewed(localDateTime);
+        menuItemReview.setComments(comments);
+
+        MenuItemReview savedMenuItemReview = MenuItemReviewRepository.save(menuItemReview);
+
+        return savedMenuItemReview;
+    }
+}
--- src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
@@ -5,7 +5,6 @@ import edu.ucsb.cs156.example.entities.MenuItemReview;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface MenuItemReviewRepository extends CrudRepository<MenuItemReview, Long> {
 }
